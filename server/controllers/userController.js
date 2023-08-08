8396197dc7c280e30a91029a5b277662ec99a9be
AlexDev2112 <AlexDev2112@gmail.com>
2023-08-08 23:40:40 +0200

    Server  4.1 (1:00:00) DEV

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,31 @@
 const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Cabinet} = require('../models/models')
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn: process.env.LIVE_TOKEN}
+    )
+}
 class UserController {
 
-    async registration(req, res) {
-
+    async registration(req, res, next) {
+        const {email, password, role} = req.body
+        if (!email || !password) {
+            return next(ApiError.badRequest('Не коректний логін чи пароль'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            return next(ApiError.badRequest('Користувач з таким догином вже існує'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const cabinet = await Cabinet.create({userId: user.id})
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
     }
 
     async login(req, res) {
