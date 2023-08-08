8fb585770f9db166d9e86f23f7e2ae7c92beb877
AlexDev2112 <AlexDev2112@gmail.com>
2023-08-09 00:20:53 +0200

    Server  5.0 (1:10:00) DEV

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -13,32 +13,56 @@ const generateJwt = (id, email, role) => {
 class UserController {
 
     async registration(req, res, next) {
+    try{
         const {email, password, role} = req.body
+        console.log('++++++++++++++++++++++')
+        console.log(email )
         if (!email || !password) {
             return next(ApiError.badRequest('Не коректний логін чи пароль'))
         }
         const candidate = await User.findOne({where: {email}})
         if (candidate) {
-            return next(ApiError.badRequest('Користувач з таким догином вже існує'))
+            return next(ApiError.badRequest('Користувач з таким логином вже існує'))
         }
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({email, role, password: hashPassword})
         const cabinet = await Cabinet.create({userId: user.id})
         const token = generateJwt(user.id, user.email, user.role)
         return res.json({token})
+    }catch(e){
+        console.log('--------------------------')
+        next(ApiError.badRequest(e.message))
+    }
     }
 
     async login(req, res) {
-
+    try{
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.internal('Користувач не знвйден'))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal('Указан невірний пароль'))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
+    }catch(e){
+        console.log('--------------------------')
+        next(ApiError.badRequest(e.message))
+    }
     }
 
     async check(req, res, next) {
-       
+    try{  
         const {id} = req.query
-        if (!id) {
-           return next(ApiError.badRequest('не задан ID'))
-        }
-        res.json(id)
+        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        return res.json({token})
+    }catch(e){
+        console.log('--------------------------')
+        next(ApiError.badRequest(e.message))
+    }
     }
 }
 
