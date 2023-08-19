94f8aadb63035ce531b5774d3e4834fa056ed4e4
AlexDev2112 <AlexDev2112@gmail.com>
2023-08-20 00:37:52 +0200

    Server + Client 7.1  (2:14:30) DEV

--- client/src/index.js
+++ client/src/index.js
@@ -1,47 +1,21 @@
-import React, {createContext} from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import UserStore from "./store/UserStore";  
+import React, { createContext } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import UserStore from "./store/UserStore";
 import TenderStore from "./store/TenderStore";
 
-export const Context = createContext(null)
+export const Context = createContext(null);
+
+console.log(process.env.REACT_APP_API_URL);
 
 ReactDOM.render(
-    <Context.Provider value={{
-        user: new UserStore(),
-        tender: new TenderStore(),
-    }}>
-        <App />
-    </Context.Provider>,
-  document.getElementById('root')
+  <Context.Provider
+    value={{
+      user: new UserStore(),
+      tender: new TenderStore(),
+    }}
+  >
+    <App />
+  </Context.Provider>,
+  document.getElementById("root")
 );
-
-
-
-//
-
-
-// import React, { createContext } from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import UserStore from './store/User.Store';
-
-// export const Context = createContext(null)
-
-
-// ReactDOM.Render(
-//   <Context.Provider value={{
-//     user: new UserStore()
-//   }}>
-//     <App />
-//   </Context.Provider>,
-//   document.getElementById('root')
-// )
-// // const root = ReactDOM.createRoot(document.getElementById('root'));
-// // root.render(
-// //   <
-// //   <React.StrictMode>
-// //     <App />
-// //   </React.StrictMode>
-// // );
-
